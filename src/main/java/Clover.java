2336b186a0463896f45c55095dacf91f464d6b2f
yan yee <yanyeeng0105@gmail.com>
2024-08-23 11:58:17 +0800

    Level-1

---
+++ src/main/java/Clover.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Clover {
+    public static void main(String[] args) {
+        //greet
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Clover");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        //exit
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
