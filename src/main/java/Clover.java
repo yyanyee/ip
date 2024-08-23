be7ebf20f891aa589802b2777fbf3c301307e77e
yan yee <yanyeeng0105@gmail.com>
2024-08-23 12:09:08 +0800

    Level-1

--- src/main/java/Clover.java
+++ src/main/java/Clover.java
@@ -6,8 +6,26 @@ public class Clover {
         System.out.println(" Hello! I'm Clover");
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-        //exit
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String command;
+
+        while (true) {
+            command = scanner.nextLine();
+
+            if (command.equals("bye")) {
+                //exit if user types the command bye
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+
+            // else, echo the command
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + command);
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close();
     }
 }
