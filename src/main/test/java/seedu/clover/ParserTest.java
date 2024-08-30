6fe061699c3670ff14dbbf247c9c2411e5c2bb5e
yan yee <yanyeeng0105@gmail.com>
2024-08-30 13:07:32 +0800

    A-JUnit

---
+++ src/main/test/java/seedu/clover/ParserTest.java
@@ -0,0 +1,13 @@
+import clover.util.Parser;
+import clover.util.Parser.Commands;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParserTest {
+
+    @Test
+    public void testParseCommand() {
+        Commands command = Parser.parse("todo read book");
+        assertEquals(Commands.TODO, command, "Command should be parsed as TODO.");
+    }
+}
