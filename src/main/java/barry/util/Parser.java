132617d0de65635bd98d8fe0c5c92977da3ff705
yan yee <yanyeeng0105@gmail.com>
2024-09-11 23:59:45 +0800

    A-CodeQuality updated

--- src/main/java/barry/util/Parser.java
+++ src/main/java/barry/util/Parser.java
@@ -6,12 +6,8 @@ import barry.command.*;
  * Represents a parser that interprets user input and returns the corresponding command.
  */
 public class Parser {
-
-    /**
-     * Enumeration for the possible commands.
-     */
     public enum CommandType {
-        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, FIND, HELP, UNKNOWN
+        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, FIND, UNKNOWN, HELP
     }
 
     /**
@@ -23,11 +19,11 @@ public class Parser {
     public static Command parse(String input) {
         assert input != null : "command cannot be null!";
         String[] split = input.split(" ", 2);
-        String command = split[0].toLowerCase();
-
+        String command = split[0].toUpperCase();
         CommandType commandType;
+
         try {
-            commandType = CommandType.valueOf(command.toUpperCase());
+            commandType = CommandType.valueOf(command);
         } catch (IllegalArgumentException e) {
             commandType = CommandType.UNKNOWN;
         }
@@ -42,60 +38,85 @@ public class Parser {
                     int i = Integer.parseInt(split[1]) - 1;
                     return new MarkCommand(i);
                 } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                    return new UnknownCommand("Unknown command!! I do not know this :(");
+                    return new UnknownCommand("bzz~ Unknown command :o There is no task listed number this index ");
                 }
             case UNMARK:
                 try {
                     int i = Integer.parseInt(split[1]) - 1;
                     return new UnmarkCommand(i);
                 } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                    return new UnknownCommand("Unknown command!! I do not know this :(");
+                    return new UnknownCommand("bzz~ Unknown command :o There is no task listed number this index ");
                 }
             case TODO:
                 if (split.length < 2 || split[1].trim().isEmpty()) {
-                    return new UnknownCommand("Unknown command!! I do not know this :( Description seems to be empty...");
+                    return new UnknownCommand("Unknown command... Description seems to be empty" +
+                            "\nPlease use the following format: todo <description> ");
                 }
                 return new TodoCommand(split[1]);
             case DEADLINE:
                 try {
                     String[] parts = split[1].split(" /by ");
                     if (parts.length < 2 || parts[0].trim().isEmpty()) {
-                        return new UnknownCommand("Unknown command!! I do not know this :( Description seems to be empty...");
+                        return new UnknownCommand("Unknown date bzz~ " +
+                                "\nPlease use the following format: deadline <description> /by <yyyy-mm-dd> ");
                     }
                     return new DeadlineCommand(parts[0].trim(), parts[1].trim());
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    return new UnknownCommand("Unknown command!! I do not know this :(");
+                    return new UnknownCommand("What is the task description? " +
+                            "\nPlease use the following format: deadline <description> /by <yyyy-mm-dd> ");
                 }
             case EVENT:
                 try {
                     String[] parts = split[1].split(" /from ");
                     if (parts.length < 2) {
-                        return new UnknownCommand("Unknown command!! I do not know this :( Description seems to be empty...");
+                        return new UnknownCommand("What is the start time and/or date? " +
+                                "\nPlease use the following format: " +
+                                "\nevent <description> /from <__ am/pm> /to <__am/pm>" +
+                                "\nOR event <description> /from <yyyy-mm-dd> /to <yyyy-mm-dd>" +
+                                "\nOR event <description> /from <yyyy-mm-dd> <__am/pm> /to <yyyy-mm-dd> <__am/pm>");
                     }
                     String[] times = parts[1].split(" /to ");
                     if (times.length < 2) {
-                        return new UnknownCommand("Unknown command!! I do not know this :( Time seems to be empty...");
+                        return new UnknownCommand("What is the end time and/or date? " +
+                                "\nPlease use the following format: " +
+                                "\nevent <description> /from <__ am/pm> /to <__am/pm>" +
+                                "\nOR event <description> /from <yyyy-mm-dd> /to <yyyy-mm-dd>" +
+                                "\nOR event <description> /from <yyyy-mm-dd> <__am/pm> /to <yyyy-mm-dd> <__am/pm>");
                     }
                     return new EventCommand(parts[0].trim(), times[0].trim(), times[1].trim());
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    return new UnknownCommand("Unknown command!! I do not know this :(");
+                    return new UnknownCommand("What is the task description? " +
+                            "\nPlease use the following format: " +
+                            "\nevent <description> /from <__ am/pm> /to <__am/pm>" +
+                            "\nOR event <description> /from <yyyy-mm-dd> /to <yyyy-mm-dd>" +
+                            "\nOR event <description> /from <yyyy-mm-dd> <__am/pm> /to <yyyy-mm-dd> <__am/pm>");
                 }
             case DELETE:
                 try {
                     int i = Integer.parseInt(split[1]) - 1;
                     return new DeleteCommand(i);
                 } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                    return new UnknownCommand("Unknown command!! I do not know this :(");
+                    return new UnknownCommand("bzz~ Unknown command :o There is no task listed under this index ");
                 }
             case FIND:
                 if (split.length < 2 || split[1].trim().isEmpty()) {
-                    return new UnknownCommand("Unknown command!! I do not know this :(");
+                    return new UnknownCommand("Unknown command..." +
+                            "\nPlease use the following format:" +
+                            "\n find <keyword>");
                 }
                 return new FindCommand(split[1].trim());
-            case HELP:
-                return new HelpCommand();
             default:
-                return new UnknownCommand("Unknown command!! I do not know this :(");
+                return new UnknownCommand("Unknown command..." +
+                        "\nHere is a list of commands:" +
+                        "\n  bye" +
+                        "\n  list" +
+                        "\n  mark" +
+                        "\n  unmark" +
+                        "\n  todo" +
+                        "\n  deadline" +
+                        "\n  event" +
+                        "\n  delete" +
+                        "\n  find");
         }
     }
 }
